3dd6e8c8192c473eea64b51678a202270b7182ec
왕호진 <kkgmco0825@naver.com>
2019-01-14 09:19:03 +0900

    no message

--- test1/test.py
+++ test1/test.py
@@ -1,86 +1,63 @@
-# 과적합 방지 기법 중 하나인 Dropout 을 사용해봅니다.
-import tensorflow as tf
+"""
+    TEST2
+    ~~~~~
+"""
 import numpy as np
-import matplotlib.pyplot as plt
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+# 신경망 함수
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-#########
-# 신경망 모델 구성
-######
-X = tf.placeholder(tf.float32, [None, 784])
-Y = tf.placeholder(tf.float32, [None, 10])
-keep_prob = tf.placeholder(tf.float32)
+def identity(x):
+    """
 
-W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
-L1 = tf.nn.relu(tf.matmul(X, W1))
-# 텐서플로우에 내장된 함수를 이용하여 dropout 을 적용합니다.
-# 함수에 적용할 레이어와 확률만 넣어주면 됩니다. 겁나 매직!!
-L1 = tf.nn.dropout(L1, keep_prob)
+    :param x:
+    :return:
+    """
+    return x
 
-W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
-L2 = tf.nn.relu(tf.matmul(L1, W2))
-L2 = tf.nn.dropout(L2, keep_prob)
+def init_network():
+    """
 
-W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
-model = tf.matmul(L2, W3)
+    :return:
+    """
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+    network = {}
+    network['W1'] = np.array([[1, 3, 5], [2, 4, 6]])  # weight 은 관습적으로 W라고 합니다.
+    network['W2'] = np.array([[1, 2], [3, 4], [5, 6]])
+    network['W3'] = np.array([[1, 2], [3, 4]])
+    return network
 
-#########
-# 신경망 모델 학습
-######
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
 
-batch_size = 100
-total_batch = int(mnist.train.num_examples / batch_size)
+# 신경망 구현
+def forward(network, x):
+    """미리 입력받은 a와 b값이 같은지 확인하여 결과를 반환합니다.
 
-for epoch in range(2):
-    total_cost = 0
+           :return: boolean True or False에 대한 결과, a와 b가 값으면 True, 다르면 False
 
-    for i in range(total_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+           예제:
+               다음과 같이 사용하세요:
 
-        _, cost_val = sess.run([optimizer, cost],
-                               feed_dict={X: batch_xs,
-                                          Y: batch_ys,
-                                          keep_prob: 0.8})
-        total_cost += cost_val
+               >>> Test(1, 2).is_same()
+               False
 
-    print('Epoch:', '%04d' % (epoch + 1),
-          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+           """
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
 
-print('최적화 완료!')
+    y = np.dot(x, W1)
+    y_hat = sigmoid(y)
+    k = np.dot(y_hat, W2)
+    k_hat = sigmoid(k)
+    j = np.dot(k_hat, W3)
+    j_hat = identity(j)
+    """identity 대신 softmax함수를 사용할 수 있습니다."""
+    return j_hat
 
-#########
-# 결과 확인
-######
-is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-print('정확도:', sess.run(accuracy,
-                        feed_dict={X: mnist.test.images,
-                                   Y: mnist.test.labels,
-                                   keep_prob: 1}))
+network = init_network()
 
-#########
-# 결과 확인 (matplot)
-######
-labels = sess.run(model,
-                  feed_dict={X: mnist.test.images,
-                             Y: mnist.test.labels,
-                             keep_prob: 1})
+x = np.array([1, 2])
 
-fig = plt.figure()
-for i in range(10):
-    subplot = fig.add_subplot(2, 5, i + 1)
-    subplot.set_xticks([])
-    subplot.set_yticks([])
-    subplot.set_title('%d' % np.argmax(labels[i]))
-    subplot.imshow(mnist.test.images[i].reshape((28, 28)),
-                   cmap=plt.cm.gray_r)
 
-plt.show()
+"""입력"""
+y = forward(network, x)  # 출력
+print(y)
